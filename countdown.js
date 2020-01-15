a92003ecbe3854e9e66aa9f1300a16ddffd9f2de
bleuarff <bleuarff@gmail.com>
2020-01-15 23:10:28 +0100

    Add style and hours & days counters

--- countdown.js
+++ countdown.js
@@ -13,10 +13,18 @@ class Countdown{
     const now = new moment(Date.now()),
           d = moment.duration(this.target.diff(now))
 
-    const display = `${d.months()} Mois ${d.days()} Jours ${d.hours()} Heures ${d.minutes()} Minutes ${d.seconds()} secondes`
-    // console.log(display)
+    const display = `${d.months()} Mois ${d.days()} Jours ${d.hours()} Heures ${d.minutes()} Minutes ${d.seconds()} secondes`,
+          asdays = d.as('days'),
+          ashours = d.as('hours')
+
     const nd = document.getElementById('countdown')
     nd.innerText = display
+
+    const ndd = document.getElementById('days')
+    ndd.innerText = asdays.toFixed(0)
+
+    const ndh = document.getElementById('hours')
+    ndh.innerText = ashours.toFixed(0)
 
   }
 }
