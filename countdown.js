2565aae5eed32e7927e8b4a0cb0fb3e6a5276193
bleuarff <bleuarff@gmail.com>
2020-01-16 21:17:18 +0100

    refactor

--- countdown.js
+++ countdown.js
@@ -1,30 +1,29 @@
 'use strict'
 
-class Countdown{
+let target
 
-  constructor(target){
-    this.target = new moment(target)
+function startCountdown(date){
+  target = new moment(date)
 
-    setInterval(() => {this.update()}, 1000)
-    this.update()
-  }
+  setInterval(update, 1000)
+  update()
+}
 
-  update(){
-    const now = new moment(Date.now()),
-          d = moment.duration(this.target.diff(now))
+function update(){
+  const now = new moment(Date.now()),
+  d = moment.duration(target.diff(now))
 
-    const display = `${d.months()} Mois ${d.days()} Jours ${d.hours()} Heures ${d.minutes()} Minutes ${d.seconds()} secondes`,
-          asdays = d.as('days'),
-          ashours = d.as('hours')
+  const display = `${d.months()} Mois ${d.days()} Jours ${d.hours()} Heures ${d.minutes()} Minutes ${d.seconds()} secondes`,
+  asdays = d.as('days'),
+  ashours = d.as('hours')
 
-    const nd = document.getElementById('countdown')
-    nd.innerText = display
+  const nd = document.getElementById('countdown')
+  nd.innerText = display
 
-    const ndd = document.getElementById('days')
-    ndd.innerText = asdays.toFixed(0)
+  const ndd = document.getElementById('days')
+  ndd.innerText = asdays.toFixed(0)
 
-    const ndh = document.getElementById('hours')
-    ndh.innerText = ashours.toFixed(0)
+  const ndh = document.getElementById('hours')
+  ndh.innerText = ashours.toFixed(0)
 
-  }
 }
